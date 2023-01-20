577c8060bd30961802bb0786bdfe13c9bcec0cf9
Edsonllima <focusti@hotmail.com>
2023-01-20 17:35:58 -0300

    add path Navbar.css

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import './Navbar.css'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -11,7 +12,7 @@ const Navbar = () => {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/new"}>Novo Post</Link>
+          <Link to={"/new"} className="new-btn">Novo Post</Link>
         </li>
       </ul>
     </nav>
